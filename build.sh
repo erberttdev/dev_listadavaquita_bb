d734d5092d5974a8bfa10ccab197836a994a4a97
erberttdev <erbertt@gmail.com>
2025-06-09 05:48:46 -0300

    requirements

--- build.sh
+++ build.sh
@@ -6,6 +6,12 @@ echo "Iniciando o build..."
 pip install -r requirements.txt
 
 # aplica as migrações
-python manage.py migrate
+
+echo "Applying migrations..."
+python manage.py migrate --noinput
+
+echo "Collecting static files..."
+python manage.py collectstatic --noinput
 
 echo "Build finalizado."
+
