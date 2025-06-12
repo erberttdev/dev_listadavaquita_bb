71a06e74a96118f8a543e25570ecac3d6d9e9255
erberttdev <erbertt@gmail.com>
2025-06-12 15:26:00 -0300

    fix: NoReverseMatch  -  url add

--- events/urls.py
+++ events/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('', views.EventListView.as_view(), name='event_list'),
     path('create/', views.EventCreateView.as_view(), name='event_create'),
     path('<int:pk>/', views.EventDetailView.as_view(), name='event_detail'),
+    path('<int:pk>/public/', views.EventPublicDetailView.as_view(), name='event_public_detail'),
     path('<int:pk>/edit/', views.EventUpdateView.as_view(), name='event_edit'),
     path('<int:pk>/delete/', views.EventDeleteView.as_view(), name='event_delete'),
 ]
