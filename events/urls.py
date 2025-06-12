dbbc5b4379756c8afbff69f290c65f76d8dabf08
erberttdev <erbertt@gmail.com>
2025-06-12 04:49:42 -0300

    fix: edição e exclusão de listas

--- events/urls.py
+++ events/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('', views.EventListView.as_view(), name='event_list'),
     path('create/', views.EventCreateView.as_view(), name='event_create'),
     path('<int:pk>/', views.EventDetailView.as_view(), name='event_detail'),
+    path('<int:pk>/edit/', views.EventUpdateView.as_view(), name='event_edit'),
+    path('<int:pk>/delete/', views.EventDeleteView.as_view(), name='event_delete'),
 ]
--- events/views.py
+++ events/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView, CreateView, DetailView
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Event
@@ -25,3 +25,20 @@ class EventDetailView(LoginRequiredMixin, DetailView):
     model = Event
     template_name = 'events/event_detail.html'
     context_object_name = 'event'
+
+class EventUpdateView(LoginRequiredMixin, UpdateView):
+    model = Event
+    fields = ['name', 'date', 'visibility']
+    template_name = 'events/event_form.html'
+    success_url = reverse_lazy('events:event_list')
+
+    def get_queryset(self):
+        return Event.objects.filter(user=self.request.user)
+
+class EventDeleteView(LoginRequiredMixin, DeleteView):
+    model = Event
+    template_name = 'events/event_confirm_delete.html'
+    success_url = reverse_lazy('events:event_list')
+
+    def get_queryset(self):
+        return Event.objects.filter(user=self.request.user)
