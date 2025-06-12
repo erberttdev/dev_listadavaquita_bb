f84e40a62c665e143d1bef1c3abc677dad83d979
erberttdev <erbertt@gmail.com>
2025-06-12 02:48:23 -0300

    fix: falha na conexão bd remoto

--- listadavaquita/settings.py
+++ listadavaquita/settings.py
@@ -81,11 +81,11 @@ WSGI_APPLICATION = 'listadavaquita.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME', 'vaquinha'),
-        'USER': os.getenv('DB_USER', 'postgres'),
-        'PASSWORD': os.getenv('DB_PASSWORD', 'postgres'),
-        'HOST': os.getenv('DB_HOST', 'localhost'),
-        'PORT': os.getenv('DB_PORT', '5432'),
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
