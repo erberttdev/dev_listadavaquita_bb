e9cf094e6b35c20264b53269e82f1004a0dfeb53
erberttdev <erbertt@gmail.com>
2025-06-13 18:32:34 -0300

    fix: add presentes - limite de caracteres no link do produto para 500

--- gifts/forms.py
+++ gifts/forms.py
@@ -8,7 +8,7 @@ class GiftForm(forms.ModelForm):
     ]
 
     store_type = forms.ChoiceField(choices=STORE_TYPE_CHOICES, label="Tipo da Loja", widget=forms.RadioSelect)
-    product_link = forms.URLField(label="Link do Produto", required=False)
+    product_link = forms.URLField(label="Link do Produto", required=False, max_length=500)
     name = forms.CharField(label="Nome do Produto", max_length=255)
     photo = forms.ImageField(label="Foto do Produto", required=False)
     value = forms.DecimalField(label="Preço do Produto", max_digits=10, decimal_places=2)
