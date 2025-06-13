ffe831472eb9f2782f71b487d6cd4adeb1807ab5
erberttdev <erbertt@gmail.com>
2025-06-13 00:54:39 -0300

    melhoria: dashboard como pg inicial, e Minhas listas

--- listadavaquita/urls.py
+++ listadavaquita/urls.py
@@ -3,11 +3,14 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from users.views import DashboardView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls', namespace='users')),
     path('accounts/', include('allauth.urls')),
-    path('', include('events.urls')),
+    path('', DashboardView.as_view(), name='dashboard'),
+    path('events/', include('events.urls')),
     path('gifts/', include('gifts.urls')),
     path('contributions/', include('contributions.urls')),
     path('payments/', include('payments.urls')),
