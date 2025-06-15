0936b00f59144aea342705d48ffd5a7b3a97d6f0
erberttdev <erbertt@gmail.com>
2025-06-14 22:11:15 -0300

    fix: deploy - catalogo

--- gifts/forms.py
+++ gifts/forms.py
@@ -1,5 +1,10 @@
 from django import forms
-from .models import GiftListItem
+from .models import GiftListItem, Gift
+
+class GiftForm(forms.ModelForm):
+    class Meta:
+        model = Gift
+        fields = ['name', 'value', 'store_name', 'store_type', 'store_address_or_link', 'photo', 'product_link', 'priority', 'allow_simultaneous_contributions']
 
 class AddToGiftListForm(forms.ModelForm):
     class Meta:
